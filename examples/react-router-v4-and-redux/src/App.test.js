12d35dec966522d8cc15e0ab6951729d68fbb001
Peter Beshai <peter.beshai@gmail.com>
2016-10-02 16:08:06 -0400

    Fix example test crashing

--- examples/react-router-v4-and-redux/src/App.test.js
+++ examples/react-router-v4-and-redux/src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Router from 'react-router/BrowserRouter';
+import { RouterToUrlQuery } from 'react-url-query';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Router>
+      <RouterToUrlQuery>
+        <App />
+      </RouterToUrlQuery>
+    </Router>, div);
 });
