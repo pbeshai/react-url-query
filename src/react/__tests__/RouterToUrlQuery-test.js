668a88c5564d6355f65c7c90af2aa563b2bb6b09
Peter Beshai <peter.beshai@gmail.com>
2017-05-23 12:07:40 +0200

    Fix missed proptype changes #15

--- src/react/__tests__/RouterToUrlQuery-test.js
+++ src/react/__tests__/RouterToUrlQuery-test.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { mount } from 'enzyme';
 import RouterToUrlQuery from '../RouterToUrlQuery';
 import urlQueryConfig from '../../urlQueryConfig';
--- src/react/addUrlProps.js
+++ src/react/addUrlProps.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { parse as parseQueryString } from 'query-string';
 
 import urlQueryDecoder from '../urlQueryDecoder';
