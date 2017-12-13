3940426dc262c0e8f6a5d24396fa3f2131447f54
kujon <jakub.korzeniowski@gmail.com>
2017-12-13 14:36:02 +0000

    JSDoc

--- src/urlQueryEncoder.js
+++ src/urlQueryEncoder.js
@@ -1,5 +1,13 @@
 import { encode } from './serialize';
 
+/**
+ * Encodes a query based on the config. Similarly to `encode`, it does not respect the `defaultValue`
+ * field, so any missing values must be specified explicitly.
+ *
+ * @param {Object} query The query object (typically from props.location.query)
+ *
+ * @return {Object} the decoded values `{ key: decodedValue, ... }`
+ */
 export default function urlQueryEncoder(config) {
   return function encodeQuery(query) {
     // encode the query
